d5fc36180f1250d8cfbc011bf12a2c7b2b83d1ac
Randini Mendis <99355199+randinimendis@users.noreply.github.com>
2023-07-29 16:13:02 +0530

    update_mini_4_commit

--- js/script.js
+++ js/script.js
@@ -1,21 +1,37 @@
+// script.js
+
 // toggle icon navbar
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
+menuIcon.onclick =()=>{
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
 
 // scroll sections
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
+  let scrollY = window.scrollY;
 
+  // Active navbar links
+  sections.forEach((sec) => {
+    let top = sec.offsetTop - 100;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
 
-    // sticky header
+    if (scrollY >= top && scrollY < top + height) {
+      navLinks.forEach((link) => {
+        link.classList.remove('active');
+      });
+      document.querySelector(`header nav a[href="#${id}"]`).classList.add('active');
+    }
+  });
 
-
-    // remove toggle icon and navbar when click navbar links (scroll)
-
-
-    // animation footer on scroll
-
-}
+  // Sticky header
+  let header = document.querySelector('header');
+  header.classList.toggle('sticky', scrollY > 100);
+};
+  
